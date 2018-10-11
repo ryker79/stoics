63e96a7aea4310ead02d5562f352b90f1ee4934c
Jeff Rice <jeff.rice@vml.com>
2018-10-11 07:37:03 -0500

    added philosopher class

---
+++ stoics/Models/Philosopher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace stoics.Models
+{
+    public class Philosopher
+    {
+        public Philosopher(string name, DateTime birthday, string orderName = null, string biographyLink = null)
+        {
+            Name = name;
+            Birthday = birthday;
+            OrderName = orderName ?? name;
+            BiographyLink = biographyLink ?? string.Empty;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string OrderName { get; set; }
+        public DateTime Birthday { get; set; }
+        public string BiographyLink { get; set; }
+    }
+}
